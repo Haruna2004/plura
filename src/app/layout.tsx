cb30c522efdfd000a4127b63189e733053f3388d
Haruna Faruk <harunafaruk2004@gmail.com>
2024-02-12 22:22:12 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider} from '@clerk/nextjs'
-import {dark } from "@clerk/themes"
+
 import { ThemeProvider } from "@/providers/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Plura",
-  description: "Run your agency, in one place",
+  description: "All in one Agency Solution",
 };
 
 export default function RootLayout({
@@ -18,9 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{baseTheme: dark}}>
+    
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={font.className}>
       <ThemeProvider attribute="class"
             defaultTheme="system"
             enableSystem
@@ -30,6 +29,5 @@ export default function RootLayout({
              </ThemeProvider>
         </body>
     </html>
-    </ClerkProvider>
   );
 }
